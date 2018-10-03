4276096f8251d23910eea140045c2759fea90c44
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-10-03 15:18:31 +0200

    INT-0063: Remove boilerplate components

---
+++ src/renderer/components/MainWindow.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="title">Request Rocket</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-window',
+  };
+</script>
+
+<style>
+</style>
