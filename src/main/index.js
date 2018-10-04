59ca1f7a319ce0142aaec53bf2044ee7a9dbbbf4
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-10-04 16:38:37 +0200

    INT-0063: Fix code-style in boilerplate code

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from 'electron' // eslint-disable-line
+import { app, BrowserWindow, ipcMain } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
 import SendRequestHandler from './send-request-handler';
 
 /**
@@ -6,7 +6,7 @@ import SendRequestHandler from './send-request-handler';
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\'); // eslint-disable-line
 }
 
 let mainWindow;
