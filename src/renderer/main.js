46b81ea11aab7c2a4ae9ed408f4169eca624df00
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-10-05 09:44:39 +0200

    INT-0063: Listen for IPC messages from main process

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,18 +1,26 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { ipcRenderer } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
 
 import App from './App';
 import router from './router';
 import createStore from './store';
+import Action from './store/action-types';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
+const store = createStore();
+
 /* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
-  store: createStore(),
+  store,
   template: '<App/>',
 }).$mount('#app');
+
+ipcRenderer.on('receive-response', (event, args) => {
+  store.dispatch(Action.receiveResponse, args);
+});
