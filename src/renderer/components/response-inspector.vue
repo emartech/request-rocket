3fe974e0d8b8587ffd31357e93c0e6669991fde9
Gabor Barnak <gabor.barnak@gmail.com>
2018-10-10 14:33:59 +0200

    INT-0066: display response headers on frontend

--- src/renderer/components/response-inspector.vue
+++ src/renderer/components/response-inspector.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
+    <h6>Response Body</h6>
     <pre id="response-body">{{ response.body }}</pre>
+    <h6>Response Headers</h6>
+    <pre id="response-headers">{{ response.headers }}</pre>
   </div>
 </template>
 
--- test/unit/specs/renderer/components/response-inspector.spec.js
+++ test/unit/specs/renderer/components/response-inspector.spec.js
@@ -1,5 +1,5 @@
 import createStore from '@/store';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ResponseInspector from '@/components/response-inspector';
 import Mutation from '@/store/mutation-types';
 import Vue from 'vue';
@@ -11,11 +11,29 @@ describe('ResponseInspector.vue', () => {
     store = createStore();
   });
 
-  it('should render correct contents', async () => {
-    store.commit(Mutation.UPDATE_RESPONSE, { body: '{"dummy":"content"}' });
+  it('should render correct body contents', async () => {
+    const ipcResponse = {
+      body: { dummy: 'content' },
+      headers: { connection: 'close' },
+    };
+    store.commit(Mutation.UPDATE_RESPONSE, ipcResponse);
     await Vue.nextTick();
-    const component = mount(ResponseInspector, { store });
+    const component = shallowMount(ResponseInspector, { store });
+    const bodyElement = component.find('pre#response-body');
+    const expectedBody = JSON.parse(bodyElement.element.textContent);
+    expect(expectedBody).to.eql(ipcResponse.body);
+  });
 
-    expect(component.find('pre#response-body').element.textContent).to.equal('{"dummy":"content"}');
+  it('should render correct header contents', async () => {
+    const ipcResponse = {
+      body: { dummy: 'content' },
+      headers: { connection: 'close' },
+    };
+    store.commit(Mutation.UPDATE_RESPONSE, ipcResponse);
+    await Vue.nextTick();
+    const component = shallowMount(ResponseInspector, { store });
+    const headersElement = component.find('pre#response-headers');
+    const expectedHeaders = JSON.parse(headersElement.element.textContent);
+    expect(expectedHeaders).to.eql(ipcResponse.headers);
   });
 });
