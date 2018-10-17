867935d2c33c794dd1a9890ae8972ea1dee1faf7
bencso <gabor.bencsik13@gmail.com>
2018-10-17 14:41:35 +0200

    INT-0068: list request headers on ui

---
+++ src/renderer/components/header-editor.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div v-for="header in headers">
+      <input
+        v-model="header.name"
+        class="header-name">
+      <input
+        v-model="header.value"
+        class="header-value">
+    </div>
+    <!-- <div class="e-grid">
+      <div class="e-cell e-cell-1">
+        <div class="doc-griditem doc-griditem-auto"/>
+      </div>
+      <div class="e-cell e-cell-5">
+        <div class="doc-griditem doc-griditem-auto">
+          Name
+        </div>
+      </div>
+      <div class="e-cell e-cell-5">
+        <div class="doc-griditem doc-griditem-auto">
+          Value
+        </div>
+      </div>
+      <div class="e-cell e-cell-1">
+        <div class="doc-griditem doc-griditem-auto"/>
+      </div>
+    </div>
+    <div
+      v-for="(header, index) in headers"
+      :key="index"
+      class="e-grid">
+      <div class="e-cell e-cell-1">
+        <div class="doc-griditem doc-griditem-auto">
+          <div class="e-field">
+            <input
+              id="cb0"
+              v-model="header.send"
+              class="e-checkbox e-checkbox-onlycheckbox"
+              type="checkbox">
+            <label for="cb0"/>
+          </div>
+        </div>
+      </div>
+      <div class="e-cell e-cell-5">
+        <div class="doc-griditem doc-griditem-auto">
+          <div class="e-field">
+            <input
+              v-model="header.name"
+              class="e-input"
+              type="text">
+          </div>
+        </div>
+      </div>
+      <div class="e-cell e-cell-5">
+        <div class="doc-griditem doc-griditem-auto">
+          <div class="e-field">
+            <input
+              v-model="header.value"
+              class="e-input"
+              type="text">
+          </div>
+        </div>
+      </div>
+      <div class="e-cell e-cell-1">
+        <div class="doc-griditem doc-griditem-auto">
+          <div class="e-btn e-btn-onlyicon">
+            <e-icon icon="trash-o"/>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'HeaderEditor',
+  computed: {
+    ...mapState({
+      headers: state => state.request.headers
+    })
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
---
+++ test/unit/specs/renderer/components/header-editor.spec.js
@@ -0,0 +1,68 @@
+import { shallowMount } from '@vue/test-utils';
+import HeaderEditor from '@/components/header-editor';
+import createStore from '@/store';
+import Mutation from '../../../../../src/renderer/store/mutation-types';
+
+describe('HeaderEditor.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    store = createStore();
+  });
+
+  it('should render inputs for content-type header by default', () => {
+    const component = shallowMount(HeaderEditor, { store });
+
+    expect(component.find('input.header-name').element.value).to.eql('content-type');
+    expect(component.find('input.header-value').element.value).to.eql('application/json');
+  });
+
+  it('should change input value for a header when header changes in store', () => {
+    const component = shallowMount(HeaderEditor, { store });
+
+    store.commit(Mutation.SET_REQUEST_HEADERS, [{ name: 'Content-Type', value: 'text/plain' }]);
+
+    expect(component.find('input.header-name').element.value).to.eql('Content-Type');
+    expect(component.find('input.header-value').element.value).to.eql('text/plain');
+  });
+
+  it('should change header name in store when header name value changes', () => {
+    const component = shallowMount(HeaderEditor, { store });
+    const headerNameInput = component.find('input.header-name');
+
+    headerNameInput.element.value = 'Content-Type';
+    headerNameInput.trigger('input');
+
+    expect(store.state.request.headers[0].name).to.eql('Content-Type');
+  });
+
+  it('should change header name in store when header name value changes', () => {
+    const component = shallowMount(HeaderEditor, { store });
+    const headerNameInput = component.find('input.header-value');
+
+    headerNameInput.element.value = 'text/plain';
+    headerNameInput.trigger('input');
+
+    expect(store.state.request.headers[0].value).to.eql('text/plain');
+  });
+
+  context('when more header item is present', () => {
+    it('should render inputs for all header elements', () => {
+      const headers = [
+        { name: 'my-header', value: 'my-header-value' },
+        { name: 'other-header', value: 'other-header-value' },
+      ];
+      store.commit(Mutation.SET_REQUEST_HEADERS, headers);
+
+      const component = shallowMount(HeaderEditor, { store });
+
+      const headerNameInputs = component.findAll('input.header-name');
+      const headerValueInputs = component.findAll('input.header-value');
+
+      headers.forEach((header, index) => {
+        expect(headerNameInputs.at(index).element.value).to.eql(header.name);
+        expect(headerValueInputs.at(index).element.value).to.eql(header.value);
+      });
+    });
+  });
+});
