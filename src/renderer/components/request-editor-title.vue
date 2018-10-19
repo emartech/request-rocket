a807c618f65e8cb353142493ef627944917b4b24
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-10-19 10:48:14 +0200

    INT-0069: Reset all settings button w/o actual functionality

--- src/renderer/components/request-editor-title.vue
+++ src/renderer/components/request-editor-title.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="e-fullheight__header">
     <h2>Request</h2>
+    <div class="e-fullheight__actions">
+      <button class="e-btn e-btn-link">
+        reset all settings
+      </button>
+    </div>
   </div>
 </template>
 
