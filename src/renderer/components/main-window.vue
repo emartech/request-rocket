f5973b32a04a2a65da173ff21293a71c1cf343e1
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-10-19 11:42:10 +0200

    INT-0069: Display errors w/ mock data

--- src/renderer/components/main-window.vue
+++ src/renderer/components/main-window.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="e-fullheight__wrapper">
+    <div
+      v-if="errorOccurred"
+      class="e-alert e-alert-fixed e-alert-withicon e-alert-danger"
+      style="top: 0">
+      <span class="e-alert__icon">
+        <!-- eslint-disable -->
+        <e-icon icon="warning"></e-icon>
+      </span>
+      Something went wrong...
+    </div>
     <div class="e-fullheight__container">
       <request-editor/>
       <response-inspector/>
