5df5c6bb1d97b4c321155e9acae7f716fa90f913
Kovács Ariel <arielkovacs@gmail.com>
2018-10-25 16:23:53 +0200

    INT-0070: Escher signer appends host header to request

--- src/main/request-signer/escher-signer.js
+++ src/main/request-signer/escher-signer.js
@@ -1,1 +1,17 @@
-export default class EscherSigner {}
+import { clone } from 'ramda';
+import { URL } from 'url';
+
+function addHostHeader(request) {
+  request.headers.host = new URL(request.url).hostname;
+}
+
+export default class EscherSigner {
+  /* eslint-disable */
+  signRequest(request) {
+    const clonedRequest = clone(request);
+
+    addHostHeader(clonedRequest);
+
+    return clonedRequest;
+  }
+}
---
+++ test/unit/specs/main/request-signer/escher-signer.spec.js
@@ -0,0 +1,19 @@
+import EscherSigner from '../../../../../src/main/request-signer/escher-signer';
+
+describe('EscherSigner', () => {
+  describe('#signRequest', () => {
+    it('should not alter the original request headers', () => {
+      const signer = new EscherSigner();
+      const request = { method: 'GET', url: 'http://some.url', headers: {} };
+      signer.signRequest(request);
+      expect(request.headers).to.eql({});
+    });
+
+    it('should add host header to request headers', () => {
+      const signer = new EscherSigner();
+      const request = { method: 'GET', url: 'https://some.url:443', headers: {} };
+      const signedRequest = signer.signRequest(request);
+      expect(signedRequest.headers.host).to.eql('some.url');
+    });
+  });
+});
