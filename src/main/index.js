93ffe2b6d05add39c5284dca6bc1f12baa10d82c
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2018-12-02 16:03:06 +0100

    INT-CHORE: Enable "Select All" on input fields

--- src/main/index.js
+++ src/main/index.js
@@ -50,7 +50,8 @@ function createWindow() {
         { type: 'separator' },
         { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
         { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' }
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
       ]
     }
   ];
