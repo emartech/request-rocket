0cefae3357c9b392d8c31cda7a52c145ed24b543
Kovács Ariel <arielkovacs@gmail.com>
2019-02-06 18:41:16 +0100

    INT-CHORE: Take version number from package.json

--- src/main/rest-client.js
+++ src/main/rest-client.js
@@ -1,5 +1,6 @@
 import { promisify } from 'util';
 import request from 'request';
+const { version } = require('../../../../package');
 
 export default class RestClient {
   constructor(timeoutInMilliseconds = 60000) {
@@ -8,7 +9,7 @@ export default class RestClient {
         timeout: timeoutInMilliseconds,
         time: true,
         headers: {
-          'user-agent': 'RequestRocket/1.1.1'
+          'user-agent': `RequestRocket/${version}`
         }
       })
     );
