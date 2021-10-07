d5baf51353379426bfc9cc9c58dae80498a16ea8
Jozsef Szabadszallasi <jozsef.szabadszallasi@emarsys.com>
2021-10-07 21:55:06 +0200

    PREDICT-OPS: due to Let's Encrypt expired root CA, need to update electron version (expired DST Root Ca removed)

--- src/main/index.js
+++ src/main/index.js
@@ -22,7 +22,8 @@ function createWindow() {
     useContentSize: true,
     width: 1000,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
